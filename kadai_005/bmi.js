009dcbef2b48235c6859c1111e54666453173b62
ikumincreate <141818014+ikumincreate@users.noreply.github.com>
2023-09-11 20:51:45 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,17 @@
+// 変数の宣言(体重)
+let bodyWeight;
+
+// 値の代入(体重)
+bodyWeight = 68;
+
+// 変数の宣言 (身長)
+let height;
+
+// 値の代入(身長)
+height = 1.7;
+
+// BMIの計算
+const bmi = bodyWeight / (height * height);
+
+// コンソールへの出力
+console.log(bmi);
