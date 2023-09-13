7cfaadb2fe4b45888aa0c3d64d989670a9356970
ikumincreate <141818014+ikumincreate@users.noreply.github.com>
2023-09-13 16:23:16 +0900

    再提出

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -4,17 +4,17 @@ let num = Math.floor(Math.random( ) * 1000);
 // 変数numの値を出力する（確認用）
 console.log(num);
 
+// 変数numの値が3と5の倍数であれば、「3と5の倍数です」と表示する
+if (num % 3 === 0 && num % 5 ===0) {
+  console.log('3と5の倍数です');
+}
 // 変数numの値が3の倍数であれば、「3の倍数です」と表示する
-if (num % 3 === 0) {
+else if (num % 3 === 0) {
   console.log('3の倍数です');
 }
 // 変数numの値が5の倍数であれば、「5の倍数です」と表示する
 else if (num % 5 === 0) {
   console.log('5の倍数です');
-}
-// 変数numの値が3と5の倍数であれば、「3と5の倍数です」と表示する
-else if (num % 3 === 0 && num % 5 ===0) {
-  console.log('3と5の倍数です');
 }
 // 変数numの値がそれ以外の場合
 else {
