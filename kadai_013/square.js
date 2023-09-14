ce9e964ec3ca9c6f468b1fc8c0b9198fd404514e
ikumincreate <141818014+ikumincreate@users.noreply.github.com>
2023-09-14 17:57:05 +0900

    再提出

--- kadai_013/square.js
+++ kadai_013/square.js
@@ -1,7 +1,5 @@
-// 引数numを二乗し、その値を出力する関数を定義する
 const squaredNumbaer = (num) => {
-  console.log(num ** 2);
+  return num ** 2;
 }
 
-// 関数を呼び出し、引数として二乗した数値を渡す
-squaredNumbaer(10);
+console.log(squaredNumbaer(10));
